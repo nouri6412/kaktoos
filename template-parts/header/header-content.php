35ec2771ccb5191631d37202c7ec2239d77b93d7
Jalil Nouri <nouri.6412@gmail.com>
2022-03-26 16:29:17 +0430

    123

---
+++ template-parts/header/header-content.php
@@ -0,0 +1,271 @@
+<body class="wt-login">
+	<!--[if lt IE 8]>
+		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+	<![endif]-->
+	<!-- Preloader Start -->
+	<div class="preloader-outer">
+		<div class="loader"></div>
+	</div>
+	<!-- Preloader End -->
+	<!-- Wrapper Start -->
+	<div id="wt-wrapper" class="wt-wrapper wt-haslayout">
+		<!-- Content Wrapper Start -->
+		<div class="wt-contentwrapper">
+			<!-- Header Start -->
+			<header id="wt-header" class="wt-header wt-haslayout">
+				<div class="wt-navigationarea">
+					<div class="container-fluid">
+						<div class="row">
+							<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+								<strong class="wt-logo"><a href="index-2.html"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="<?php echo get_bloginfo('name'); ?>"></a></strong>
+								<div class="wt-rightarea">
+									<nav id="wt-nav" class="wt-nav navbar-expand-lg">
+										<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+											<i class="lnr lnr-menu"></i>
+										</button>
+										<div class="collapse navbar-collapse wt-navigation" id="navbarNav">
+											<ul class="navbar-nav">
+												<li class="menu-item-has-children page_item_has_children">
+													<a href="javascript:void(0);">اصلی</a>
+													<ul class="sub-menu">
+														<li class="menu-item-has-children page_item_has_children wt-notificationicon"><span class="wt-dropdowarrow"><i class="lnr lnr-chevron-left"></i></span>
+															<a href="javascript:void(0);">صفحه اصلی</a>
+															<ul class="sub-menu">
+																<li><a href="index-2.html">صفحه اصلی 1</a></li>
+																<li class="wt-newnoti"><a href="indexvtwo.html">صفحه اصلی 2<em>بدون ورود به سیستم</em></a></li>
+															</ul>
+														</li>
+														<li class="menu-item-has-children page_item_has_children"><span class="wt-dropdowarrow"><i class="lnr lnr-chevron-left"></i></span>
+															<a href="javascript:void(0);">مقاله</a>
+															<ul class="sub-menu">
+																<li><a href="articlelist.html">لیست مقاله</a></li>
+																<li><a href="articlegrid.html">شبکه مقاله</a></li>
+																<li><a href="articlesingle.html">مقاله مجرد</a></li>
+																<li><a href="articleclassic.html">مقاله کلاسیک</a></li>
+															</ul>
+														</li>
+														<li class="menu-item-has-children page_item_has_children"><span class="wt-dropdowarrow"><i class="lnr lnr-chevron-left"></i></span>
+															<a href="javascript:void(0);">شرکت</a>
+															<ul class="sub-menu">
+																<li><a href="companygrid.html">شبکه شرکت</a></li>
+																<li><a href="companysigle.html">شرکت مجرد</a></li>
+															</ul>
+														</li>
+														<li>
+															<a href="about.html">درباره</a>
+														</li>
+														<li>
+															<a href="privacypolicy.html">سیاست حفظ حریم خصوصی</a>
+														</li>
+														<li>
+															<a href="comingsoon.html">به زودی</a>
+														</li>
+														<li>
+															<a href="404page.html">صفحه 404</a>
+														</li>
+													</ul>
+												</li>
+												<li class="nav-item">
+													<a href="howitworks.html">چگونه کار می کند</a>
+												</li>
+												<li class="menu-item-has-children page_item_has_children">
+													<a href="javascript:void(0);">مرور مشاغل</a>
+													<ul class="sub-menu">
+														<li>
+															<a href="joblisting.html">لیست مشاغل</a>
+														</li>
+														<li class="current-menu-item">
+															<a href="jobsingle.html">مشاغل مجرد</a>
+														</li>
+														<li>
+															<a href="jobproposal.html">پیشنهاد شغل</a>
+														</li>
+													</ul>
+												</li>
+												<li class="menu-item-has-children page_item_has_children">
+													<a href="javascript:void(0);">مشاهده فریلنسرها</a>
+													<ul class="sub-menu">
+														<li>
+															<a href="userlisting.html">لیست کاربران</a>
+														</li>
+														<li class="current-menu-item">
+															<a href="usersingle.html">کاربر مجرد</a>
+														</li>
+													</ul>
+												</li>
+											</ul>
+										</div>
+									</nav>
+									<?php
+									if (!is_user_logged_in()) {
+									?>
+										<div style="display: block;" class="wt-loginarea">
+											<figure class="wt-userimg">
+												<img src="<?php echo get_template_directory_uri(); ?>/assets/images/user-login.png" alt="img description">
+											</figure>
+											<div class="wt-loginoption">
+												<a href="javascript:void(0);" id="wt-loginbtn" class="wt-loginbtn">ورود به سیستم</a>
+												<div class="wt-loginformhold">
+													<div class="wt-loginheader">
+														<span>ورود به سیستم</span>
+														<a href="javascript:;"><i class="fa fa-times"></i></a>
+													</div>
+													<div class="wt-formtheme wt-loginform do-login-form">
+														<fieldset>
+															<div class="box-loading">
+																<div class="loading-ajax"></div>
+															</div>
+															<div id="dzFormMsg-error" class="dzFormMsg error"></div>
+															<div id="dzFormMsg-doned" class="dzFormMsg doned"></div>
+															<div class="form-group">
+																<input type="text" id="user_login" name="user_login" class="form-control" placeholder="نام کاربری">
+															</div>
+															<div class="form-group">
+																<input type="password" id="user_pass" name="user_pass" class="form-control" placeholder="رمز عبور">
+															</div>
+															<div class="wt-logininfo">
+
+																<button onclick="ajax_submit_mbm_login(
+										$('#user_login').val()
+										,$('#user_pass').val()
+										,$('#dzFormMsg-error')
+                                        ,$('#dzFormMsg-doned'))" type="button" class="wt-btn do-login-button">ورود به سیستم </button>
+
+
+																<span class="wt-checkbox">
+																	<input id="wt-login" type="checkbox" name="rememberme">
+																	<label for="wt-login">اطلاعات مرا به خاطر بسپار</label>
+																</span>
+															</div>
+														</fieldset>
+														<div class="wt-loginfooterinfo">
+															<a href="javascript:;" class="wt-forgot-password">فراموشی رمز عبور</a>
+															<a href="register.html">ایجاد حساب</a>
+														</div>
+													</div>
+													<form class="wt-formtheme wt-loginform do-forgot-password-form wt-hide-form">
+														<fieldset>
+															<div class="form-group">
+																<input type="email" name="email" class="form-control get_password" placeholder="ایمیل">
+															</div>
+
+															<div class="wt-logininfo">
+																<a href="javascript:;" class="wt-btn do-get-password">دریافت رمز عبور</a>
+															</div>
+														</fieldset>
+														<div class="wt-loginfooterinfo">
+															<a href="javascript:void(0);" class="wt-show-login">ورود به سیستم</a>
+															<a href="register.html">ایجاد حساب</a>
+														</div>
+													</form>
+												</div>
+											</div>
+											<a href="register.html" class="wt-btn">اکنون بپیوندید</a>
+										</div>
+									<?php
+									} else {
+									?>
+										<div style="display: block;" class="wt-userlogedin">
+											<figure class="wt-userimg">
+												<img src="<?php echo get_template_directory_uri(); ?>/assets/images/user-img.jpg" alt="image description">
+											</figure>
+											<div class="wt-username">
+												<h3> پریسا محمدی</h3>
+												<span>پریسا محمدی</span>
+											</div>
+											<nav class="wt-usernav">
+												<ul>
+													<li class="menu-item-has-children page_item_has_children">
+														<a href="javascript:void(0);">
+															<span>بینش </span>
+														</a>
+														<ul class="sub-menu children">
+															<li><a href="dashboard-insights.html">بینش ها</a></li>
+															<li><a href="dashboard-insightsuser.html">بینش کاربر</a></li>
+														</ul>
+													</li>
+													<li>
+														<a href="dashboard-profile.html">
+															<span>پروفایل من</span>
+														</a>
+													</li>
+													<li class="menu-item-has-children">
+														<a href="javascript:void(0);">
+															<span>همه مشاغل</span>
+														</a>
+														<ul class="sub-menu">
+															<li><a href="dashboard-completejobs.html">مشاغل تکمیل شده</a></li>
+															<li><a href="dashboard-canceljobs.html">مشاغل لغو شده</a></li>
+															<li><a href="dashboard-ongoingjob.html">مشاغل مداوم</a></li>
+															<li><a href="dashboard-ongoingsingle.html">مجرد مداوم</a></li>
+														</ul>
+													</li>
+													<li>
+														<a href="dashboard-managejobs.html">
+															<span>مدیریت مشاغل</span>
+														</a>
+													</li>
+													<li class="wt-notificationicon menu-item-has-children">
+														<a href="javascript:void(0);">
+															<span>پیام ها</span>
+														</a>
+														<ul class="sub-menu">
+															<li><a href="dashboard-messages.html">پیام ها</a></li>
+															<li><a href="dashboard-messages2.html">پیام ها 2</a></li>
+														</ul>
+													</li>
+													<li>
+														<a href="dashboard-saveitems.html">
+															<span> موارد ذخیره شده من </span>
+														</a>
+													</li>
+													<li>
+														<a href="dashboard-invocies.html">
+															<span>فاکتورها </span>
+														</a>
+													</li>
+													<li>
+														<a href="dashboard-category.html">
+															<span>دسته بندی</span>
+														</a>
+													</li>
+													<li>
+														<a href="dashboard-packages.html">
+															<span>بسته ها</span>
+														</a>
+													</li>
+													<li>
+														<a href="dashboard-proposals.html">
+															<span>پیشنهادات</span>
+														</a>
+													</li>
+													<li>
+														<a href="dashboard-accountsettings.html">
+															<span>تنظیمات حساب</span>
+														</a>
+													</li>
+													<li>
+														<a href="dashboard-helpsupport.html">
+															<span>راهنما و پشتیبانی</span>
+														</a>
+													</li>
+													<li>
+														<a href="index-2.html">
+															<span> خروج از سیستم</span>
+														</a>
+													</li>
+												</ul>
+											</nav>
+										</div>
+									<?php
+									}
+									?>
+
+
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
+			<!--Header End-->
