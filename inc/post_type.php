085e8070f4995166f571d610e924302ac4119231
Jalil Nouri <nouri.6412@gmail.com>
2022-03-26 16:18:02 +0430

    8

--- inc/post_type.php
+++ inc/post_type.php
@@ -45,49 +45,6 @@ function kaktos_post_type_contact()
 }
 add_action('init', 'kaktos_post_type_contact');
 
-///post type our-service
-
-function kaktos_post_type_service()
-{
-
-    $supports = array(
-        'title', // post title
-        'thumbnail', // featured images
-		'editor',
-		'excerpt',
-        'custom-fields', // custom fields
-        'post-formats', // post formats
-		
-    );
-
-    $labels = array(
-        'name' => _x('خدمات ما', 'plural'),
-        'singular_name' => _x('خدمات ما', 'singular'),
-        'menu_name' => _x('خدمات ما', 'admin menu'),
-        'name_admin_bar' => _x('خدمات ما', 'admin bar'),
-        'add_new' => _x('ثبت خدمات ما جدید', 'add new'),
-        'add_new_item' => "ثبت خدمات ما جدید",
-        'new_item' => "خدمات ما جدید",
-        'edit_item' => "ویرایش خدمات ما",
-        'view_item' => "مشاهده خدمات ما",
-        'all_items' => "همه خدمات ما ها",
-        'search_items' => "جستجوی خدمات ما",
-        'not_found' => "یافت نشد"
-    );
-
-    $args = array(
-        'supports' => $supports,
-        'labels' => $labels,
-        'public' => true,
-        'query_var' => true,
-        'rewrite' => array('slug' => 'our-service'),
-        'has_archive' => true,
-        'hierarchical' => false,
-    );
-    register_post_type('our-service', $args);
-}
-add_action('init', 'kaktos_post_type_service');
-
 
 ///post type job
 
@@ -105,17 +62,17 @@ function kaktos_post_type_job()
     );
 
     $labels = array(
-        'name' => _x('آگهی شغل', 'plural'),
-        'singular_name' => _x(' آگهی شغل', 'singular'),
-        'menu_name' => _x('آگهی شغل', 'admin menu'),
-        'name_admin_bar' => _x('آگهی شغل', 'admin bar'),
-        'add_new' => _x('ثبت آگهی شغل جدید', 'add new'),
-        'add_new_item' => "ثبت  آگهی شغل جدید",
-        'new_item' => " آگهی شغل جدید",
-        'edit_item' => "ویرایش  آگهی شغل",
-        'view_item' => "مشاهده  آگهی شغل",
-        'all_items' => "همه  آگهی شغل ها",
-        'search_items' => "جستجوی  آگهی شغل",
+        'name' => _x('پروژه', 'plural'),
+        'singular_name' => _x(' پروژه', 'singular'),
+        'menu_name' => _x('پروژه', 'admin menu'),
+        'name_admin_bar' => _x('پروژه', 'admin bar'),
+        'add_new' => _x('ثبت پروژه جدید', 'add new'),
+        'add_new_item' => "ثبت  پروژه جدید",
+        'new_item' => " پروژه جدید",
+        'edit_item' => "ویرایش  پروژه",
+        'view_item' => "مشاهده  پروژه",
+        'all_items' => "همه  پروژه ها",
+        'search_items' => "جستجوی  پروژه",
         'not_found' => "یافت نشد"
     );
 
@@ -219,51 +176,6 @@ function kaktos_post_type_city()
 add_action('init', 'kaktos_post_type_city');
 
 
-///post type category company
-
-function kaktos_post_type_company_cat()
-{
-
-    $supports = array(
-        'title', // post title
-        'thumbnail', // featured images
-		'editor',
-		'excerpt',
-        'custom-fields', // custom fields
-        'post-formats', // post formats
-		
-    );
-
-    $labels = array(
-        'name' => _x('دسته شرکت ها', 'plural'),
-        'singular_name' => _x('دسته شرکت ها', 'singular'),
-        'menu_name' => _x('دسته شرکت ها', 'admin menu'),
-        'name_admin_bar' => _x('دسته شرکت ها', 'admin bar'),
-        'add_new' => _x('ثبت  دسته شرکت ها', 'add new'),
-        'add_new_item' => "ثبت  دسته شرکت ها جدید",
-        'new_item' => " دسته شرکت ها  جدید",
-        'edit_item' => "ویرایش  دسته شرکت ها ",
-        'view_item' => "مشاهده  دسته شرکت ها",
-        'all_items' => "همه  دسته شرکت ها ها",
-        'search_items' => "جستجوی   دسته شرکت ها",
-        'not_found' => "یافت نشد"
-    );
-
-    $args = array(
-        'supports' => $supports,
-        'labels' => $labels,
-        'public' => true,
-        'query_var' => true,
-        'rewrite' => array('slug' => 'company-cat'),
-        'has_archive' => true,
-        'hierarchical' => false,
-    );
-    register_post_type('company-cat', $args);
-}
-add_action('init', 'kaktos_post_type_company_cat');
-
-///post type category company
-
 function kaktos_post_type_job_cat()
 {
 
@@ -278,17 +190,17 @@ function kaktos_post_type_job_cat()
     );
 
     $labels = array(
-        'name' => _x('دسته شغل ها', 'plural'),
-        'singular_name' => _x('دسته شغل ها', 'singular'),
-        'menu_name' => _x('دسته شغل ها', 'admin menu'),
-        'name_admin_bar' => _x('دسته شغل ها', 'admin bar'),
-        'add_new' => _x('ثبت  دسته شغل ها', 'add new'),
-        'add_new_item' => "ثبت  دسته شغل ها جدید",
-        'new_item' => " دسته شغل ها  جدید",
-        'edit_item' => "ویرایش  دسته شغل ها ",
-        'view_item' => "مشاهده  دسته شغل ها",
-        'all_items' => "همه  دسته شغل ها ها",
-        'search_items' => "جستجوی   دسته شغل ها",
+        'name' => _x('دسته پروژه ها', 'plural'),
+        'singular_name' => _x('دسته پروژه ها', 'singular'),
+        'menu_name' => _x('دسته پروژه ها', 'admin menu'),
+        'name_admin_bar' => _x('دسته پروژه ها', 'admin bar'),
+        'add_new' => _x('ثبت  دسته پروژه ها', 'add new'),
+        'add_new_item' => "ثبت  دسته پروژه ها جدید",
+        'new_item' => " دسته پروژه ها  جدید",
+        'edit_item' => "ویرایش  دسته پروژه ها ",
+        'view_item' => "مشاهده  دسته پروژه ها",
+        'all_items' => "همه  دسته پروژه ها ها",
+        'search_items' => "جستجوی   دسته پروژه ها",
         'not_found' => "یافت نشد"
     );
 
@@ -321,17 +233,17 @@ function kaktos_post_type_job_tag()
     );
 
     $labels = array(
-        'name' => _x('برچسب شغل', 'plural'),
-        'singular_name' => _x('برچسب شغل', 'singular'),
-        'menu_name' => _x('برچسب شغل', 'admin menu'),
-        'name_admin_bar' => _x('برچسب شغل', 'admin bar'),
-        'add_new' => _x('ثبت برچسب شغل جدید', 'add new'),
-        'add_new_item' => "ثبت  برچسب شغل جدید",
-        'new_item' => " برچسب شغل  جدید",
-        'edit_item' => "ویرایش  برچسب شغل ",
-        'view_item' => "مشاهده  برچسب شغل",
-        'all_items' => "همه  برچسب شغل ها",
-        'search_items' => "جستجوی   برچسب شغل",
+        'name' => _x('برچسب پروژه', 'plural'),
+        'singular_name' => _x('برچسب پروژه', 'singular'),
+        'menu_name' => _x('برچسب پروژه', 'admin menu'),
+        'name_admin_bar' => _x('برچسب پروژه', 'admin bar'),
+        'add_new' => _x('ثبت برچسب پروژه جدید', 'add new'),
+        'add_new_item' => "ثبت  برچسب پروژه جدید",
+        'new_item' => " برچسب پروژه  جدید",
+        'edit_item' => "ویرایش  برچسب پروژه ",
+        'view_item' => "مشاهده  برچسب پروژه",
+        'all_items' => "همه  برچسب پروژه ها",
+        'search_items' => "جستجوی   برچسب پروژه",
         'not_found' => "یافت نشد"
     );
 
