2f528fe39cebf514e70678f55d6732dee124592e
Home Pc <nouri.6412@gmail.com>
2022-04-13 19:29:52 +0430

    1111

--- page-bot.php
+++ page-bot.php
@@ -11,9 +11,9 @@
  * Template Name: ربات
  */
 $str=file_get_contents("php://input");
-$myfile = fopen("hook-bot.txt", "w") or die("Unable to open file!");
-fwrite($myfile, $str);
-fclose($myfile);
+// $myfile = fopen("hook-bot.txt", "w") or die("Unable to open file!");
+// fwrite($myfile, $str);
+// fclose($myfile);
 
 $update = json_decode($str, TRUE,512,JSON_UNESCAPED_UNICODE);
 
